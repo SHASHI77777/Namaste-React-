6608d38a4218709e853275226ad31b9f146a056b
shashi <shashircciit07@gmail.com>
2024-05-06 02:47:32 +0530

    chapter-01

---
+++ CHAPTER 1 /App.js
@@ -0,0 +1,33 @@
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [
+     React.createElement(
+     "div",
+       { id: "child" },
+        [React.createElement("h1", {},"i'm am h1 tag"),
+        React.createElement("h1", {},"i'm am h2 tag"),
+    ]),
+        React.createElement(
+        "div",
+          { id: "child2" },
+           [
+            React.createElement("h1", {},"i'm am h3 tag"),
+           React.createElement("h1", {},"i'm am h4 tag"),
+       ]),
+    ]);
+ 
+console.log(parent);
+
+const heading = React.createElement(
+ "h1", 
+{ id:"heading",xyz:"abc"}, 
+"Hello world from React!"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
+ root.render(parent);
+
